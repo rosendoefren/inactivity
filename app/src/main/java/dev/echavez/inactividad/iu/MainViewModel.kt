6c706b0fea0f1508eea91aa67d47e3669391618c
rosendoefren <isc.efrenchavez@gmail.com>
2023-03-07 15:37:28 -0600

    Fragment, Handler, Runnable

--- app/src/main/java/dev/echavez/inactividad/iu/MainViewModel.kt
+++ app/src/main/java/dev/echavez/inactividad/iu/MainViewModel.kt
@@ -1,25 +1,43 @@
 package dev.echavez.inactividad.iu
 
 import android.os.Handler
+import android.os.Looper
+import android.os.SystemClock
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
 
     val sessionActive = MutableLiveData<Boolean>()
-    lateinit var mHandler: Handler
-    lateinit var mRunnable: Runnable
+    val rTime = MutableLiveData<Long>()
+    private var mHandler: Handler? = null
+    private var mRunnable: Runnable? = null
     var mTime: Long = 10000 // 10 seconds
 
 
     // start handler function
     fun startHandler(){
-        mHandler.postDelayed(mRunnable, mTime)
+        if(mHandler == null){
+            mHandler = Handler(Looper.getMainLooper())
+            mRunnable = Runnable {
+                stopHandler()
+                sessionActive.value = false
+                mHandler = null
+                mRunnable = null
+            }
+            mHandler?.postDelayed(mRunnable!!, mTime)
+            rTime.value = SystemClock.elapsedRealtime() + mTime
+        }
     }
 
     // stop handler function
     fun stopHandler(){
-        mHandler.removeCallbacks(mRunnable)
+        mHandler!!.removeCallbacks(mRunnable!!)
+    }
+
+    fun updateTimer(){
+        rTime.value = SystemClock.elapsedRealtime() + mTime
+        mHandler?.postDelayed(mRunnable!!, mTime)
     }
 }
 
--- app/src/main/java/dev/echavez/inactividad/iu/second/SecondFragment.kt
+++ app/src/main/java/dev/echavez/inactividad/iu/second/SecondFragment.kt
@@ -8,16 +8,20 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
+import androidx.core.os.HandlerCompat.hasCallbacks
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import dev.echavez.inactividad.R
 import dev.echavez.inactividad.databinding.FragmentSecondBinding
 import dev.echavez.inactividad.iu.MainViewModel
+import kotlinx.coroutines.android.asCoroutineDispatcher
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
+@RequiresApi(Build.VERSION_CODES.N)
+@SuppressLint("ClickableViewAccessibility")
 class SecondFragment : Fragment() {
 
     private val viewModel: MainViewModel by activityViewModels()
@@ -27,63 +31,72 @@ class SecondFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
-
+        init()
         return binding.root
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
-    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_MOVE) {
-                // Removes the handler callbacks (if any)
-                viewModel.stopHandler()
-
-                // Runs the handler (for the specified time)
-                // If any touch or motion is detected before
-                // the specified time, this override function is again called
-                start()
-            }
-            true
-        }
-
-        //Init
-        viewModel.mHandler = Handler(Looper.getMainLooper())
-        viewModel.mRunnable = Runnable {
-            binding.viewTimer.stop()
-            viewModel.sessionActive.value = false
-        }
-        start()
-        observer()
+        updateChronometer()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        viewModel.stopHandler()
         _binding = null
+    }
+
+    private fun init(){
+        observer()
+        listener()
+        viewModel.startHandler()
     }
 
     private fun observer(){
         viewModel.sessionActive.observe(viewLifecycleOwner){
-            if(!it){
-                findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-            }
+            if(!it) findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+        viewModel.rTime.observe(viewLifecycleOwner){
+            if(it != null) updateChronometer()
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
+    private fun listener(){
+        binding.root.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_MOVE) {
+                // Removes the handler callbacks (if any)
+                viewModel.stopHandler()
+                // Runs the handler (for the specified time)
+                // If any touch or motion is detected before
+                // the specified time, this override function is again called
+                viewModel.startHandler()
+                viewModel.updateTimer()
+            }
+            true
+        }
+    }
+
+    private fun updateChronometer(){
+        if(viewModel.rTime.value!! > SystemClock.elapsedRealtime()){
+            binding.viewTimer.isCountDown = true
+            binding.viewTimer.base = SystemClock.elapsedRealtime() + (viewModel.rTime.value!! - SystemClock.elapsedRealtime())
+            binding.viewTimer.start()
+        }
+    }
+
+
+
+
+
+    /*@RequiresApi(Build.VERSION_CODES.N)
     private fun start(){
         viewModel.startHandler()
         binding.viewTimer.isCountDown = true
         binding.viewTimer.base = SystemClock.elapsedRealtime() + viewModel.mTime
         binding.viewTimer.start()
-    }
+    }*/
 }
